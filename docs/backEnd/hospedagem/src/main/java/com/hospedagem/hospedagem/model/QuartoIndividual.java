221a10dbdfd1e60d62b658ca6dea9aa1bb654cc5
Charles Henrique de Paula Santos <charleshps.dev@gmail.com>
2026-05-17 23:19:46 -0300

    criação do metodo calcularValor do quarto Individual

--- docs/backEnd/hospedagem/src/main/java/com/hospedagem/hospedagem/model/QuartoIndividual.java
+++ docs/backEnd/hospedagem/src/main/java/com/hospedagem/hospedagem/model/QuartoIndividual.java
@@ -5,6 +5,35 @@ public class QuartoIndividual extends Quarto{
     private int numCamas;
     private double valorCama;
 
+    public QuartoIndividual(){
+
+    }
+
+    public QuartoIndividual(String tipo, double valorBase, boolean possuiAr, boolean possuiHidro, int numCamas, double valorCama){
+        super();
+        setTipo(tipo);
+        setValorBase(valorBase);
+        setPossuiAr(possuiAr);
+        setPossuiHidro(possuiHidro);
+
+        this.numCamas = numCamas;
+        this.valorCama = valorCama;
+    }
+
+    public double CalcularValor(){
+        
+        double valorTotal = 0, valorBase = getValorBase();
+
+        if (numCamas == 1){
+            valorTotal = valorBase;
+        }else if(numCamas > 1){
+            valorTotal = (valorBase + (valorCama * numCamas));
+        }
+
+        return valorTotal;
+
+    }
+
     public int getNumCamas() {
         return numCamas;
     }
